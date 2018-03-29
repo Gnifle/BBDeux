86fd7e84e7b7f17b9ff935dd5ba2a33ce96a5bcd
Daniel Olsen <github@gnifle.com>
2018-03-29 13:02:23 +0200

    Fix User model namespace app-wide

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
+/**
+ * @mixin \Eloquent
+ */
 class User extends Authenticatable
 {
     use Notifiable;
